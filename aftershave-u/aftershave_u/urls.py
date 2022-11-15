f9c016a9d36bd19c8b46e1cf4b0006a152be7adc
BXS-Archipelago <bxs@tutanota.com>
2022-11-15 12:35:57 +0000

    Modify detail.html, Add new Category and Creators Model and Admin register

--- aftershave-u/aftershave_u/urls.py
+++ aftershave-u/aftershave_u/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from aftershave_u.views import index, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', about, name='about'),
+    path('', include('blog.urls')),
     path('', index, name='index'),
 ]
